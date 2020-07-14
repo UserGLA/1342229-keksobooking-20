ee7cc2e46c88f35ffdeaf44b921c5fd6f7f331bc
UserGLA <tigr1488@yandex.ru>
2020-07-14 13:48:53 +0700

    поправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,59 +1,81 @@
 'use strict';
 
 var MIN_X = 50;
-var MIN_Y = 70;
+var MIN_Y = 130;
 var MAX_X = 1150;
-var MAX_Y = 634;
-var CHEKIN = ['12:00', '13:00', '14:00'];
-var CHCKOUT = ['12:00', '13:00', '14:00'];
-var number = ['01', '02', '03', '04', '05', '06', '07', '08'];
-var characteristic = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var type = ['palace', 'flat', 'house', 'bungalo'];
-var photo = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MAX_Y = 630;
+var MOCK_TIMES = ['12:00', '13:00', '14:00'];
+var MOCK_AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var MOCK_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var MOCK_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var MOCK_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var author = [];
-
-for (var i = 0; i < number.length; i++) {
-  var avatar = 'img/avatars/user' + number[i] + '.png';
-  author[i] = avatar;
-}
-
-function getRandomNumber(min, max) {
+var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 var getRandomStroke = function (data) {
   var rand = Math.floor(Math.random() * data.length);
   return data[rand];
 };
 
-var offer = {
-  title: 'заголовок предложения',
-  address: getRandomNumber(MIN_X, MAX_X) + ',' + getRandomNumber(MIN_Y, MAX_Y),
-  price: 1000,
-  type: getRandomStroke(type),
-  rooms: getRandomNumber(1, 3),
-  checking: getRandomStroke(CHEKIN),
-  checkout: getRandomStroke(CHCKOUT),
-  features: getRandomStroke(characteristic),
-  description: 'строка с описанием',
-  photos: getRandomStroke(photo)
+var generatePin = function (amount) {
+  var pinsArray = [];
+
+  for (var i = 0; i < amount; i++) {
+    pinsArray[i] = {
+      author: {
+        avatar: 'img/avatars/user' + MOCK_AVATARS[i] + '.png'
+      },
+      offer: {
+        title: 'заголовок предложения',
+        address: {
+          locationX: 'getRandomNumber(MIN_X, MAX_X)',
+          locationY: 'getRandomNumber(MIN_Y, MAX_Y)'
+        },
+        price: 1000,
+        type: getRandomStroke(MOCK_TYPES),
+        rooms: 3,
+        guests: 5,
+        checkin: getRandomStroke(MOCK_TIMES),
+        checkout: getRandomStroke(MOCK_TIMES),
+        features: getRandomStroke(MOCK_FEATURES),
+        description: 'описание',
+        photos: getRandomStroke(MOCK_PHOTOS)
+      },
+      location: {
+        x: getRandomNumber(MIN_X, MAX_X),
+        y: getRandomNumber(MIN_Y, MAX_Y)
+      }
+    };
+  }
+  return pinsArray;
 };
 
-var MAP = document.querySelector('.map');
-MAP.classList.remove('map--faded');
+var pins = generatePin(8);
 
-var ListElement = document.querySelector('.map__pins');
-var PinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var Pinimg = PinTemplate.querySelector('img');
+var mapElement = document.querySelector('.map');
+mapElement.classList.remove('map--faded');
 
-for (i = 0; i < number.length - 1; i++) {
-  var Pin = PinTemplate.cloneNode(true);
-  Pin.style.left = getRandomNumber(MIN_X, MAX_X) + 'px';
-  Pin.style.top = getRandomNumber(MIN_Y, MAX_Y) + 'px';
-  Pinimg.src = author[i];
-  Pinimg.alt = offer.title;
+var listElement = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinimg = pinTemplate.querySelector('img');
 
-  ListElement.appendChild(Pin);
-}
+var createPin = function (pinsarr) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style.left = pinsarr.location.x + 'px';
+  pin.style.top = pinsarr.location.y + 'px';
+  pinimg.src = pinsarr.author.avatar;
+  pinimg.alt = pinsarr.offer.title;
+
+  return pin;
+};
+
+var renderPins = function (container, data) {
+  for (var i = 0; i < data.length; i++) {
+    container.appendChild(createPin(data[i]));
+  }
+};
+
+renderPins(listElement, pins);
 
